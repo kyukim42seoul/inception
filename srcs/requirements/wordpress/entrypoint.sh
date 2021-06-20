ecc9a585dd7cb7763fa043bdfc2e83e98760a785
kyukim <kyukim@student.42seoul.kr>
2021-06-20 18:37:21 +0900

    UPDATE : change php-fpm root && dir, rename each container in .yml

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -4,5 +4,5 @@ tar -xvf latest.tar.gz
 mv wordpress WordPress
 mv wp-config.php /WordPress/wordpress/
 mv www.conf etc/php/7.3/fpm/pool.d/
-chown -R nginx:nginx /WordPress
+chown -R www-data:www-data /WordPress
 php-fpm7.3 -F -R
