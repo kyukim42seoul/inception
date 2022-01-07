db0ce0c71035933129fb2c5cb59ac13f07b33c50
kyukim <kyukim@student.42seoul.kr>
2022-01-07 15:33:30 +0900

    UPDATE : mv db.sql to var/lib/mysql/temp for check

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-FILE=/var/lib/mysql/mysql
+FILE=/var/lib/mysql/temp/db.sql
 if [ ! -e "$FILE" ]; then
 	cp -rp data/mysql.cnf etc/mysql/conf.d/
 	cp -rp mariadb.cnf etc/mysql
 	service mysql start
 	mysql < db.sql
+	mv db.sql var/lib/mysql/temp/db.sql
 	service mysql stop
 	cp -rp my.cnf etc/mysql/
 fi
