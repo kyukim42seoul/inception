47d1bcddb3e1d24c78816ee4e4be402aa2463206
kyukim <kyukim@student.42seoul.kr>
2022-01-07 16:09:48 +0900

    UPDATE : change mariadb volume root to subdirectory

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-FILE=/var/lib/mysql/temp/db.sql
 cp -rp data/mysql.cnf etc/mysql/conf.d/
 cp -rp mariadb.cnf etc/mysql
 cp -rp my.cnf etc/mysql/
-if [ ! -e "$FILE" ]; then
-	service mysql start
-	mysql < db.sql
-	service mysql stop
-	mkdir var/lib/mysql/temp
-	mv db.sql var/lib/mysql/temp/db.sql
-fi
+service mysql start
+mysql < db.sql
+service mysql stop
+mkdir var/lib/mysql/wordpress
 mysqld
