73d5b3dd505c8e311a41daf044b04db7e5f358ca
kyukim <kyukim@student.42seoul.kr>
2022-01-20 19:09:02 +0900

    UPDATE : modify mariaDB entrypoint.sh

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 FILE=/var/lib/mysql/check.txt
-if [ ! -e "$FILE" ]; then
-	touch /var/lib/mysql/check.txt
 #	mkdir var/lib/mysql/wordpress
 #	cp -rp data/mysql.cnf etc/mysql/conf.d/
-	cp -rp mariadb.cnf etc/mysql
-	cp -rp my.cnf etc/mysql/
+cp -rp mariadb.cnf etc/mysql
+cp -rp my.cnf etc/mysql/
+if [ ! -e "$FILE" ]; then
+	touch /var/lib/mysql/check.txt
 	service mysql start
 	mysql < db.sql
 	service mysql stop
